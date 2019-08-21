af44f8b3e1131fb18fbd5bc83c10d5d08ef480b4
AzureAD\SanthiyaArulsamy <Crusher@8>
2019-08-21 14:59:51 +0530

    Added the sample

---
+++ CanvasControl_DrawingSession/CanvasControl_DrawingSession/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CanvasControl_DrawingSession
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        CanvasBitmap CanvasBitmap;
+        HueRotationEffect canvasEffect;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Visibility = Visibility.Visible;
+        }
+
+        private void Canv_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
+        {
+            args.DrawingSession.Units = CanvasUnits.Pixels;
+            args.DrawingSession.DrawImage(canvasEffect);
+
+        }
+        private void Canv_CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
+        {
+            args.TrackAsyncAction(CreateResourcesAsync(sender).AsAsyncAction());
+        }
+        async Task CreateResourcesAsync(CanvasControl sender)
+        {
+            CanvasBitmap = await CanvasBitmap.LoadAsync(sender, "Flower1.jpg");
+            canvasEffect = new HueRotationEffect();
+            canvasEffect.Source = CanvasBitmap;
+            canvasEffect.Angle = 250;
+        }
+    }
+}
